add0d4f2becaac3912a812b0839e33f862c88683
gukaixin <kaixineverydaygu@outlook.com>
2023-01-03 10:53:33 +0800

    Initial commit

---
+++ charcount/charcount/src/main/java/charcount.java
@@ -0,0 +1,54 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class charcount {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        Job job =Job.getInstance(conf,charcount.class.getName());
+        job.setJarByClass(charcount.class);
+        FileInputFormat.setInputPaths(job,args[0]);
+
+        job.setMapperClass(charcountmap.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setReducerClass(charcountreduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        job.waitForCompletion(true);
+    }
+
+    public static class charcountmap extends Mapper<LongWritable,Text,Text,LongWritable>{
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            char[] splited = line.replaceAll(" ","").toCharArray();
+            for(char word : splited){
+                context.write(new Text(String.valueOf(word)),new LongWritable(1));
+            }
+        }
+    }
+    public static class charcountreduce extends Reducer<Text,LongWritable,Text,LongWritable>{
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long count = 0L;
+            for (LongWritable v : values){
+                count += v.get();
+            }
+            LongWritable v2 = new LongWritable(count);
+            context.write(key,v2);
+        }
+
+    }
+}
